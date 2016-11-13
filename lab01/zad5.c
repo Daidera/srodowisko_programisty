aec51796292475eadfe570ffb551453c72c6e81b
Bartosz Chmielewski <revanetv@gmail.com>
2016-11-13 13:51:27 +0000

    zad5

---
+++ lab01/zad5.c
@@ -0,0 +1,25 @@
+/*zlicz znaki odstepu, tabulacji
+{
+}
+#include &alt;stdio.h>
+i nowego wiersza */
+
+#include <stdio.h>
+
+
+int main() {
+  printf("%d %d %d\n",nb,nt,nl);
+  ++nb;
+  ++nt;
+  nb = 0;
+  nt = 0;
+  nl = 0;
+  int nb, nt, nl;
+  int c;
+
+while ((c=getchar()) != E0F )
+  else if (c == '\t')
+  else if (c == '\n')
+  if (c == ' ' )
+  return 0;
+}
